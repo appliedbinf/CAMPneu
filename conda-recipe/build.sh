dfea19b3ef46775961cd73cabda59bab3b2c7ddd
Mahika Kadam <109224543+mmkadam@users.noreply.github.com>
2024-09-12 03:46:10 -0400

    Update build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
  
+# $PYTHON -m pip install . -vv
+ 
 mkdir -p $PREFIX/bin/
  
-cp $RECIPE_DIR/../CAMPneu.nf $RECIPE_DIR/../23SsnpAnalysis.py $PREFIX/bin/
+cp $RECIPE_DIR/../CAMPneu.nf $RECIPE_DIR/../23SsnpAnalysis.py  $RECIPE_DIR/../environment.yml  $RECIPE_DIR/../nextflow.config $PREFIX/bin/
 
-chmod +x $PREFIX/bin/CAMPneu.nf $PREFIX/bin/23SsnpAnalysis.py 
+chmod +x $PREFIX/bin/CAMPneu.nf $PREFIX/bin/23SsnpAnalysis.py $PREFIX/bin/environment.yml $PREFIX/bin/nextflow.config
