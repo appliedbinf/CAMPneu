9a25938a049d34f9d6173a4423e3d8aab0077ac8
Mahika Kadam <109224543+mmkadam@users.noreply.github.com>
2024-09-13 10:50:05 -0400

    Update 23SsnpAnalysis.py

--- 23SsnpAnalysis.py
+++ 23SsnpAnalysis.py
@@ -12,19 +12,19 @@ if reference in vcf:
     result = subprocess.Popen(f"bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\n' {vcf} | head -1 | cut -f 1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = result.communicate()
     chrom = stdout.decode().strip()
-    filename_all = vcf.replace(".fna.sorted.vcf.gz","all23S.subset.vcf")
-    filename_reg = vcf.replace(".fna.sorted.vcf.gz","identified.snps.vcf")
+    filename_all = vcf.replace(".fna.sorted.vcf.gz","all23S.subset.txt")
+    filename_reg = vcf.replace(".fna.sorted.vcf.gz","identified.snps.txt")
     subprocess.run(f"bcftools view -r {chrom}:{start}-{end} --no-header {vcf} > {filename_all}", shell=True)
     subprocess.run(f"bcftools view -R {regions_file} --no-header {vcf} > {filename_reg}", shell=True)
     
 #
 #SNPS to look for are
-#snp1 = C217T
-#snp2 = T1112G
-#snp3 = A2063G
-#snp4 = A2064G
-#snp5 = A2431G  
-#snp6 = C2611G
-
-
-
+#snp1 = C217T      NC_000912.1	120272	120273
+#snp2 = T1112G     NC_000912.1	121167	121168
+#snp3 = A2063G/T/C NC_000912.1	122118	122119
+#snp4 = A2064G     NC_000912.1	122119	122120
+#snp5 = A2067G     NC_000912.1  122122  122123
+#snp6 = A2431G     NC_000912.1	122486	122487
+#snp7 = C2611G     NC_000912.1	122666	122667
+#snp8 = C2617A     NC_000912.1  122672  122673
+#snp9 = 
